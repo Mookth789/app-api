5864b417db7bbeb07ad304a0a215c11e57a1303c
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-07-07 11:27:37 +0700

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://gmth99:Gmth99.@gmth.ayzty.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log('DB Connected')
     } catch (err) {
         console.log(err)
